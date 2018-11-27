8b84449ba7c09da6f7d29be3ab0d3a339ef6e697
Darien Morrison <dmorr041@fiu.edu>
2018-11-26 20:23:15 -0500

    Tried dynamically printing the three reviews for each business ID, but that throws an error - Yelp limits the Queries Per Second to a pretty low number, so we'll have to find a workaround. The Yelp Knowledge API allows for this, but we need to request access, which I've done... Hopefully they respond in time.

--- Nomaste/countMiami.js
+++ Nomaste/countMiami.js
@@ -13,32 +13,60 @@ var lat;
 var long;
 var username = 'dmorr041';
 var comments;
+var businessIDs = [];
 
-client.search({
-    term:'restaurants',
-    location: 'miami',
-    limit: 50
-}).then(response => {
-    for(var j = 0; j < 50; j++){
-        var businessID      = response.jsonBody.businesses[j].id;
-        name            = response.jsonBody.businesses[j].name;
-        image           = response.jsonBody.businesses[j].image_url;
-        address         = response.jsonBody.businesses[j].location.address1 + ', '                      
-                                + response.jsonBody.businesses[j].location.city + ', ' + response.jsonBody.businesses[j].location.state    
-                                + ' ' + response.jsonBody.businesses[j].location.zip_code
-        lat             = response.jsonBody.businesses[j].coordinates.latitude;
-        long            = response.jsonBody.businesses[j].coordinates.longitude;
+function getMiamiRestaurantData() {
+    client.search({
+        term:'restaurants',
+        location: 'miami',
+        limit: 50
+    }).then(response => {
+        for(var j = 0; j < 50; j++){
+            var businessID      = response.jsonBody.businesses[j].id;
+            businessIDs.push(businessID);
+            console.log(businessIDs[j]);
+            name                = response.jsonBody.businesses[j].name;
+            image               = response.jsonBody.businesses[j].image_url;
+            address             = response.jsonBody.businesses[j].location.address1 + ', '                      
+                                    + response.jsonBody.businesses[j].location.city + ', ' + response.jsonBody.businesses[j].location.state    
+                                    + ' ' + response.jsonBody.businesses[j].location.zip_code
+            lat                 = response.jsonBody.businesses[j].coordinates.latitude;
+            long                = response.jsonBody.businesses[j].coordinates.longitude;
+    
+            console.log(j + ':')
+            console.log('Name/Title of Post: ' + name);           // Business Name
+            console.log('Image URL: ' + image);      // Image URL
+            console.log('Address: ' + address);
+            console.log('Latitude: ' + lat);               // Lat
+            console.log('Longitude: ' + long + '\n');      // Long
+        }
+        
+    }).catch(e => {
+        console.log(e);
+    });
+}
 
-        console.log(j + ':')
-        console.log('Name/Title of Post: ' + name);           // Business Name
-        console.log('Image URL: ' + image);      // Image URL
-        console.log('Address: ' + address);
-        console.log('Latitude: ' + lat);               // Lat
-        console.log('Longitude: ' + long);      // Long
-    }
-}).catch(e => {
-    console.log(e);
-});
+function getReviews(businessID) {
+    client.reviews(businessID).then(response => {
+        console.log('Comment #1: ' + response.jsonBody.reviews[0].text);
+        console.log('Comment #2: ' + response.jsonBody.reviews[1].text);
+        console.log('Comment #3: ' + response.jsonBody.reviews[2].text);
+    }).catch(e => {
+        console.log(e);
+    });
+}
+
+// console.log('BEFORE REVIEWS')
+// for(var n = 0; n < businessIDs.length; n++) {
+//     client.reviews(businessIDs[n]).then(response => {
+//         console.log('"Caption" Data: ' + response.jsonBody.reviews[0].text);
+//         console.log('Comment 1: ' + response.jsonBody.reviews[1].text);
+//         console.log('Comment 2: ' + response.jsonBody.reviews[2].text);
+//     }).catch(e => {
+//         console.log(e);
+//     });
+// }
+// console.log('AFTER REVIEWS')
 
 // for(var i = 50; i < 1000; i++){
 //     client.search({
